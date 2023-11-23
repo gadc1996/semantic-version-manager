4a8470d0c738ca1019019182ee0555d7fdd368c3
gdelgado <gdelgado@controlla.com.mx>
2023-11-23 23:02:12 +0000

    fix error

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from semantic_versioning import Version
 
 setup(
     name='semantic-versioning',
-    version= "0.0.5"
+    version= "0.0.6",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
